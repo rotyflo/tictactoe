0d94c60c3574c1d8ee2882b07310fdc399e15e93
RT <me@rtflores.com>
2021-10-08 16:46:21 -0700

    remove sass, restructure settings, rewrite logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,10 @@
 "use strict";
 
-let p1 = "";
-let p2 = "";
-let turn = "p1";
-let first = "p1";
 let message = document.getElementById("message");
 let replay = document.getElementById("replay");
 let positionsNames = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
 let positionsFunctions = [a1, a2, a3, b1, b2, b3, c1, c2, c3];
 let gameOver = false;
-let pvp = document.getElementById("pvp");
-let pvcButton = document.getElementById("pvc-button");
-let pvpButton = document.getElementById("pvp-button");
 let winningCombos = [
   [a1, a2, a3],
   [b1, b2, b3],
@@ -22,9 +15,23 @@ let winningCombos = [
   [a1, b2, c3],
   [c1, b2, a3]
 ];
+let players = {
+  one: {
+    name: "Player",
+    piece: "X",
+    turn: true,
+    winner: false
+  },
+  two: {
+    name: "Computer",
+    piece: "O",
+    turn: false,
+    winner: false
+  }
+}
 
-["x", "o"].forEach(function (piece) {
-  selectPiece(document.getElementById(piece), piece);
+document.getElementById("start-button").addEventListener("click", () => {
+  startGame();
 });
 
 positionsNames.forEach(function (position) {
@@ -32,62 +39,94 @@ positionsNames.forEach(function (position) {
 });
 
 replay.addEventListener("click", function () {
-  positionsFunctions.forEach(function (position) {
-    position.innerText = "";
-  });
+  clearBoard();
 
-  first = first === "p1" ? "p2" : "p1";
-
-  if (first === "p2") {
-    if (pvp.checked) {
-      turn = "p2";
-      message.innerText = "Player 2";
-    } else {
-      computersTurn();
-      turn = "p1";
-      message.innerText = "Player 1";
-    }
-  } else {
-    turn = "p1";
-    message.innerText = "Player 1";
+  if (getWinner().name === getCurrentPlayer().name) {
+    switchTurns();
+  } else if (players.two.name === "Computer") {
+    takeComputerTurn();
   }
+  printMatchup();
 
   gameOver = false;
   replay.style.display = "none";
 });
 
-function selectPiece(piece, symbol) {
-  piece.addEventListener("click", function () {
-    p1 = symbol.toUpperCase();
-    p2 = p1 === "X" ? "O" : "X";
 
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("game").style.display = "initial";
+// Functions
+
+function applySettings() {
+  let nameOne = document.getElementById("playerone").value;
+  let nameTwo = document.getElementById("playertwo").value;
+  if (nameOne.length > 0) players.one.name = nameOne;
+  if (nameTwo.length > 0) players.two.name = nameTwo;
+}
+
+function startGame() {
+  applySettings();
+  hideSettings();
+  printMatchup();
+  unhideBoard();
+}
+
+function hideSettings() {
+  document.getElementById("settings").style.display = "none";
+}
+
+function unhideBoard() {
+  document.getElementById("game").style.display = "block";
+  replay.style.display = "none";
+}
+
+function printMatchup() {
+  message.innerText = `${players.one.name} vs. ${players.two.name}`;
+}
+
+function switchTurns() {
+  let playerOne = players.one;
+  let playerTwo = players.two;
+
+  if (playerOne.turn) {
+    playerTwo.turn = true;
+    playerOne.turn = false;
+  } else {
+    playerOne.turn = true;
+    playerTwo.turn = false;
+  }
+}
+
+function clearBoard() {
+  positionsFunctions.forEach(function (position) {
+    position.innerText = "";
   });
+}
+
+function getWinner() {
+  if (players.one.winner) return players.one;
+  return players.two;
+}
+
+function getCurrentPlayer() {
+  if (players.one.turn) return players.one;
+  return players.two;
 }
 
 function whenClick(position) {
   position.addEventListener("click", function () {
     if (position.innerText === "" && !gameOver) {
-      position.innerText = turn === "p1" ? p1 : p2;
-      turn = turn === "p1" ? "p2" : "p1";
+      position.innerText = getCurrentPlayer().piece;
 
+      switchTurns();
       checkForWinner();
       checkForDraw();
 
       if (gameOver === false) {
-        // TWO PLAYER
-        if (pvp.checked) {
-          message.innerText = turn === "p1" ? "Player 1" : "Player 2";
-        }
-        // VS COMPUTER
-        else {
-          if (turn === "p2") {
-              computersTurn();
-          }
+        if (players.two.turn && players.two.name === "Computer") {
+          takeComputerTurn();
         }
       }
 
+      printMatchup();
       checkForWinner();
       checkForDraw();
     }
@@ -102,19 +141,19 @@ function checkForWinner() {
       result += position.innerText;
     });
 
-    if (result === "XXX") declareWinner("X");
-    if (result === "OOO") declareWinner("O");
+    if (result === "XXX") declareWinner(players.one);
+    if (result === "OOO") declareWinner(players.two);
   });
 }
 
-function declareWinner(winner) {
-  if (pvp.checked) {
-    message.innerText = p1 === winner ? "PLAYER 1 WINS!" : "PLAYER 2 WINS!";
-  }
-  else {
-    message.innerText = p1 === winner ? "PLAYER 1 WINS!" : "COMPUTER WINS!";
-  }
+function declareWinner(player) {
+  message.innerText = `${player.name} wins!`;
+  
+  players.one.winner = false;
+  players.two.winner = false;
+  player.winner = true;
 
+  switchTurns();
   gameOver = true;
   replay.style.display = "initial";
 }
@@ -127,12 +166,14 @@ function checkForDraw() {
   });
 
   if (draw && !gameOver) {
-    message.innerText = "IT'S A DRAW!";
+    message.innerText = "It's a draw!";
     replay.style.display = "initial";
   }
 }
 
-function computersTurn() {
+function takeComputerTurn() {
+  let computerPiece = players.two.piece;
+  let playerOnePiece = players.one.piece;
   let moved = false;
 
   winningCombos.forEach(function (combo) {
@@ -146,19 +187,19 @@ function computersTurn() {
       switch (result) {
         case " XX":
         case " OO":
-          combo[0].innerText = p2;
+          combo[0].innerText = computerPiece;
           moved = true;
           break;
 
         case "X X":
         case "O O":
-          combo[1].innerText = p2;
+          combo[1].innerText = computerPiece;
           moved = true;
           break;
 
         case "XX ":
         case "OO ":
-          combo[2].innerText = p2;
+          combo[2].innerText = computerPiece;
           moved = true;
           break;
       }
@@ -168,33 +209,33 @@ function computersTurn() {
   if (!moved) {
     switch (b2.innerText) {
       case "":
-        b2.innerText = p2;
+        b2.innerText = computerPiece;
         break;
 
-      case p2:
-        if (a2.innerText === "") a2.innerText = p2;
-        else if (b1.innerText === "") b1.innerText = p2;
-        else if (b3.innerText === "") b3.innerText = p2;
-        else if (c2.innerText === "") c2.innerText = p2;
+      case computerPiece:
+        if (a2.innerText === "") a2.innerText = computerPiece;
+        else if (b1.innerText === "") b1.innerText = computerPiece;
+        else if (b3.innerText === "") b3.innerText = computerPiece;
+        else if (c2.innerText === "") c2.innerText = computerPiece;
         else {
           for (let i = 0; !moved; i++) {
             if (positionsFunctions[i].innerText === "") {
-              positionsFunctions[i].innerText = p2;
+              positionsFunctions[i].innerText = computerPiece;
               moved = true;
             }
           }
         }
         break;
 
-      case p1:
-        if (a1.innerText === "") a1.innerText = p2;
-        else if (a3.innerText === "") a3.innerText = p2;
-        else if (c1.innerText === "") c1.innerText = p2;
-        else if (c3.innerText === "") c3.innerText = p2;
+      case playerOnePiece:
+        if (a1.innerText === "") a1.innerText = computerPiece;
+        else if (a3.innerText === "") a3.innerText = computerPiece;
+        else if (c1.innerText === "") c1.innerText = computerPiece;
+        else if (c3.innerText === "") c3.innerText = computerPiece;
         else {
           for (let i = 0; !moved; i++) {
             if (positionsFunctions[i].innerText === "") {
-              positionsFunctions[i].innerText = p2;
+              positionsFunctions[i].innerText = computerPiece;
               moved = true;
             }
           }
@@ -203,5 +244,5 @@ function computersTurn() {
     }
   }
 
-  turn = "p1";
+  switchTurns();
 }
